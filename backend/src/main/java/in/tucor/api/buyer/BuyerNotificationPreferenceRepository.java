12343c00f4812779cca1608b8cdcae03e8e8948b
madhu-verma29 <vermamadhu2910@gmail.com>
2026-09-18 19:10:34 +0530

    Add buyer notification preference repository

---
+++ backend/src/main/java/in/tucor/api/buyer/BuyerNotificationPreferenceRepository.java
@@ -0,0 +1,1 @@
+package in.tucor.api.buyer; import org.springframework.data.jpa.repository.JpaRepository; import java.util.UUID; public interface BuyerNotificationPreferenceRepository extends JpaRepository<BuyerNotificationPreference,UUID>{}
