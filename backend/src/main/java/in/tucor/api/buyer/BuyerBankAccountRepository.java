f96db00c2396a49b81846955dff711d804455af5
madhu-verma29 <vermamadhu2910@gmail.com>
2026-09-18 17:17:20 +0530

    Implement buyer bank account APIs

---
+++ backend/src/main/java/in/tucor/api/buyer/BuyerBankAccountRepository.java
@@ -0,0 +1,1 @@
+package in.tucor.api.buyer;import org.springframework.data.jpa.repository.JpaRepository;import java.util.*;public interface BuyerBankAccountRepository extends JpaRepository<BuyerBankAccount,UUID>{List<BuyerBankAccount> findByBuyerIdOrderByCreatedAtAsc(UUID buyerId);}
